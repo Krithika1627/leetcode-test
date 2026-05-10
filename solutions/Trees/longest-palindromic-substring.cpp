40f7910ec8066b70f0712b69ff367648ac9937af
Krithika V <krithika1627@gmail.com>
2026-05-10 18:46:30 +0530

    [Trees] longest-palindromic-substring · cpp · update

--- solutions/Trees/longest-palindromic-substring.cpp
+++ solutions/Trees/longest-palindromic-substring.cpp
@@ -1,21 +1,21 @@
-        while (left >= 0 && right < s.size() && s
-[left] == s[right]) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
             left--;
             right++;
         }
-        return s.substr(left + 1, right - left - 
-1);
+        return s.substr(left + 1, right - left - 1);
     }
 
     string longestPalindrome(string s) {
         string result = "";
 
         for (int i = 0; i < s.size(); i++) {
-            string odd = expand(s, i, 
-i);       // odd length
-            string even = expand(s, i, i + 
-1);  // even length
+            string odd = expand(s, i, i);       // odd length
+            string even = expand(s, i, i + 1);  // even length
 
-            if (odd.size() > result.size()) 
-result = odd;
-            if (even.size() > result.size()) 
+            if (odd.size() > result.size()) result = odd;
+            if (even.size() > result.size()) result = even;
+        }
+
+        return result;
+    }
+};
