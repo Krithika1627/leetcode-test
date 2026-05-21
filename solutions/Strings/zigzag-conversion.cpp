f2d73eeff2ddff8ec23228899d25175e47ce23b7
Krithika V <krithika1627@gmail.com>
2026-05-21 20:22:43 +0530

    [Strings] zigzag-conversion · cpp · update

--- solutions/Strings/zigzag-conversion.cpp
+++ solutions/Strings/zigzag-conversion.cpp
@@ -1,22 +1,29 @@
-
-        for (char c : s) {
-            rows[currentRow] += c;
-
-            // Change direction at top/bottom
-            if (currentRow == 0 || currentRow == numRows - 1)
-                goingDown = !goingDown;
-
-            currentRow += goingDown ? 1 : -1;
-        }
-
-        string result = "";
-        int currentRow = 0;
-        bool goingDown = false;
-
-        vector<string> rows(numRows);
-
-        if (numRows == 1 || s.size() <= numRows)
-            return s;
-    string convert(string s, int numRows) {
-class Solution {
+class Solution {
 public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || s.size() <= numRows)
+            return s;
+
+        vector<string> rows(numRows);
+
+        int currentRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currentRow] += c;
+
+            // Change direction at top/bottom
+            if (currentRow == 0 || currentRow == numRows - 1)
+                goingDown = !goingDown;
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        string result = "";
+
+        for (string row : rows)
+            result += row;
+
+        return result;
+    }
+};
